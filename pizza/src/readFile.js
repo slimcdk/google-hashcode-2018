458d11681209d51f53b45a88c258798da9f54196
Christian Skjerning <cs@slimc.dk>
2018-02-28 17:32:36 +0100

    simple file writer

--- pizza/src/readFile.js
+++ pizza/src/readFile.js
@@ -1,22 +1,31 @@
-let fs = require('fs'), filename = process.argv[2];
 console.log("running script");
 
+let fs = require('fs');
+let filename = process.argv[2];
 
 // Make sure we got a filename on the command line.
 if (process.argv.length < 3) {
-  console.log('Usage: node ' + process.argv[1] + ' FILENAME');
-  process.exit(1);
+    console.log('Usage: node ' + process.argv[1] + ' FILENAME');
+    process.exit(1);
 }
 
 // Read the file and print its contents.
-fs.readFile(filename, 'utf8', function(err, data) {
-  if (err) throw err;
-  console.log('OK: ' + filename);
-  console.log(data);
-  writeFile(data);
+fs.readFile(filename, 'utf8', function (err, data) {
+    if (err) throw err;
+    console.log('OK: ' + filename);
+    console.log(data);
+    writeFile(data);
 });
 
 
-function writeFile() {
+function writeFile(data) {
+    console.log("writing file..");
 
+    fs.appendFile(new Date().getTime() + '.out', data, (err) => {
+        if (err) {
+            console.error("failed!")
+        } else {
+            console.log("done!");
+        }
+    });
 }
